894b554ebbd40c066ad549517b103d042a9ca74c
killgrabber <niklas.thieme@outlook.de>
2021-11-10 17:17:10 +0100

    Add file skeleton

---
+++ VHDL/README.md
@@ -0,0 +1,5 @@
+Benutzung des start.sh scripts.
+
+Unter Ubuntu:
+
+./start.sh file1.vhdl file2.vhdl ... file_tb.vhdl
---
+++ VHDL/start.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+abort() #is executed on an error so the script stops
+{
+    echo "An error occurred. Exiting..."
+    exit 1
+}
+
+trap 'abort' 0
+
+set -e # exit on error flag
+
+for file in "$@" #handle all paramter files
+do
+echo "start hapra script..."
+if ghdl -s $file ; then
+    echo "Syntax ok"
+else
+    echo "Syntax failed"
+    exit 1
+fi
+if ghdl -a $file ; then
+    echo "Anaysis ok"
+else
+    echo "Anaysis failed"
+    exit 1
+fi
+unit=${file%.vhdl}
+ echo "unit is $unit"
+if ghdl -e $unit ; then
+    echo "Build ok"
+else
+    echo "Build failed"
+    exit 1
+fi
+if ghdl -r $unit --vcd=testbench.vcd ; then
+    echo "Testbench dump ok"
+else
+    echo "Testbench dump failed"
+    exit 1
+fi
+
+echo "start gtkwave..."
+gtkwave testbench.vcd  --rcvar 'do_initial_zoom_fit yes' &
+
+trap : 0
+
+done
