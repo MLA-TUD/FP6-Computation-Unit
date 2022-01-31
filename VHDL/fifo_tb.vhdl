5380550608a9be445a5a3f22fe5cc6cf66bc816a
M-Eniac <gero.gruehn@tu-dortmund.de>
2022-01-31 15:53:55 +0100

    fifo testbench added first example

--- VHDL/fifo_tb.vhdl
+++ VHDL/fifo_tb.vhdl
@@ -17,17 +17,59 @@ architecture test of fifo_tb is
     );
 	end component fifo;
 	
-	-- ?
-begin
-    signal rdata: std_logic_vector(dataSize-1 downto 0);
+	signal rdata: std_logic_vector(7 downto 0);
     signal wfull: std_logic;
     signal rempty: std_logic;
-    signal wdata: std_logic_vector(dataSize-1 downto 0);
+    signal wdata: std_logic_vector(7 downto 0);
     signal winc, wclk, wrst_n: std_logic;
     signal rinc, rclk, rrst_n: std_logic;
-	fi_fo: fifo generic map(8 => dataSize, 2 => addressSize) port map(rdata, wfull, rempty, wdata, winc, wclk, wrst_n,rinc, rclk, rrst_n);
+begin
+    
+	fi_fo: fifo generic map(dataSize => 8, addressSize => 2) port map(rdata, wfull, rempty, wdata, winc, wclk, wrst_n,rinc, rclk, rrst_n);
 	process begin
-		-- ?
+        rinc <= '0';
+        winc <= '0';
+        rclk <= '0';
+        
+        --reset synchronisation
+        wrst_n <= '1';
+        rrst_n <= '1';
+        wait for 1 ns;
+        wrst_n <= '0'; 
+        rrst_n <= '0';
+        wait for 1 ns;
+        wrst_n <= '1';
+        rrst_n <= '1';
+
+        -- One cycle for clean startup
+        wait for 1 ns;wclk <= '1';rclk <= '1';wait for 1 ns; wclk <= '0';rclk <= '0';
+
+		wdata <= "00000000";
+        winc <= '1';
+        wait for 1 ns;wclk <= '1';rclk <= '1';wait for 1 ns; wclk <= '0';rclk <= '0';
+        
+        wdata <= "00000001";
+        wait for 1 ns;wclk <= '1';rclk <= '1';wait for 1 ns; wclk <= '0';rclk <= '0';
+
+        wdata <= "00000010";
+        wait for 1 ns;wclk <= '1';rclk <= '1';wait for 1 ns; wclk <= '0';rclk <= '0';
+
+        wdata <= "00000011";
+        wait for 1 ns;wclk <= '1';rclk <= '1';wait for 1 ns; wclk <= '0';rclk <= '0';
+       
+
+        -- read out for several cycles
+        winc <= '0';
+        rinc <= '1';
+        wait for 1 ns;wclk <= '1';rclk <= '1';wait for 1 ns; wclk <= '0';rclk <= '0';
+        wait for 1 ns;wclk <= '1';rclk <= '1';wait for 1 ns; wclk <= '0';rclk <= '0';
+        wait for 1 ns;wclk <= '1';rclk <= '1';wait for 1 ns; wclk <= '0';rclk <= '0';
+        wait for 1 ns;wclk <= '1';rclk <= '1';wait for 1 ns; wclk <= '0';rclk <= '0';
+        wait for 1 ns;wclk <= '1';rclk <= '1';wait for 1 ns; wclk <= '0';rclk <= '0';
+        wait for 1 ns;wclk <= '1';rclk <= '1';wait for 1 ns; wclk <= '0';rclk <= '0';
+        wait for 1 ns;wclk <= '1';rclk <= '1';wait for 1 ns; wclk <= '0';rclk <= '0';
+        
+        
 		wait;
 	end process;
 end test;
