528f5c2e3117368cbbbc30920efa906f26528ec0
Timo <94067343+SirPlep@users.noreply.github.com>
2022-03-16 18:28:15 +0100

    Fix sa: mac cabels

--- VHDL/sa.vhdl
+++ VHDL/sa.vhdl
@@ -9,8 +9,8 @@ entity sa is -- systolic array
 		size : positive := 8
 	);
 	port ( -- c: clock; r: reset
-		a : in std_1d_vector_array; -- upper side
-		b : in std_1d_vector_array; -- left side
+		a : in std_1d_vector_array; -- in top
+		b : in std_1d_vector_array; -- in left
 		c : in std_logic;
 		r : in std_logic;
 		d : out std_2d_vector_array
@@ -20,13 +20,13 @@ end sa;
 architecture behavior of sa is
 	component mac is -- multiply-accumulate unit
 		port ( -- c: clock; r: reset
-			a : in std_logic_vector(7 downto 0); --eingang 1
-			b : in std_logic_vector(7 downto 0); --eingang 2
+			a : in std_logic_vector(7 downto 0); -- in top
+			b : in std_logic_vector(7 downto 0); -- in left
 			c : in std_logic; 
 			r : in std_logic;
-			d : out std_logic_vector(7 downto 0); --output mac 
-			e : out std_logic_vector(7 downto 0); -- right out
-            f : out std_logic_vector(7 downto 0) --bottom out
+			d : out std_logic_vector(7 downto 0); -- output mac
+			e : out std_logic_vector(7 downto 0); -- out bottom
+            f : out std_logic_vector(7 downto 0) -- out right
 		);
 	end component mac;
 	
@@ -34,16 +34,14 @@ architecture behavior of sa is
 	signal ver : std_2d_vector_array_length9;
 	
 begin
-    
-    
-   gen1 : FOR i IN 0 TO size-1 GENERATE
-    --fill first lines
-    hor(0, i) <= a(i);
-    ver(i, 0) <= b(i);
-      gen2 : FOR j IN 0 TO size-1 GENERATE
-        mac1 : mac port map(a => hor(i, j), b => ver(i,j), c => c, r => r, d => d(i, j), e => hor(i+1, j+1), f => ver(i+1, j+1) );
-      END GENERATE;  
-   END GENERATE;
-    
-	-- ?
+	gen1 : FOR i IN 0 TO size-1 GENERATE
+		--fill first lines
+		ver(i, 0) <= a(i);
+		hor(0, i) <= b(i); -- normally hor(0, j), but the sa is quadratic in size and only travels in one direction
+		
+		gen2 : FOR j IN 0 TO size-1 GENERATE
+			mac1 : mac port map(a => ver(i, j), b => hor(i,j), c => c, r => r, d => d(i, j), e => ver(i, j+1), f => hor(i+1, j) );
+		END GENERATE;
+	END GENERATE;
+	
 end behavior;
