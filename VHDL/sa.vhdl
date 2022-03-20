39a82348fe6e2059d85200546b43cdfcc593b9b2
M-Eniac <gero.gruehn@tu-dortmund.de>
2022-03-20 16:29:54 +0100

    sa sizes

--- VHDL/sa.vhdl
+++ VHDL/sa.vhdl
@@ -11,29 +11,29 @@ entity sa is -- systolic array
         bitSize:integer := 8                                                        -- fixed to 8 for this project
     );    
 	port (
-		upperInputVectors : in std_1d_vector_array(0 to array_size-1);              -- input 1: (top)	1-dimensional array of: 8-bit signed (2k) data
-        leftInputVectors : in std_1d_vector_array(0 to array_size-1);               -- input 2: (left)	1-dimensional array of: 8-bit signed (2k) data
+		upperInputVectors : in std_1d_vector_array(0 to systolicArraySize-1);              -- input 1: (top)	1-dimensional array of: 8-bit signed (2k) data
+        leftInputVectors : in std_1d_vector_array(0 to systolicArraySize-1);               -- input 2: (left)	1-dimensional array of: 8-bit signed (2k) data
         clk : in std_logic;                                                         -- clock
         reset : in std_logic;                                                       -- reset (each cell (-> d) to "00000000")
-        outMatrix : out std_2d_vector_array(0 to array_size-1, 0 to array_size-1)   -- output: (back)	2-dimensional array of: 8-bit signed (2k) data
+        outMatrix : out std_2d_vector_array(0 to systolicArraySize-1, 0 to systolicArraySize-1)   -- output: (back)	2-dimensional array of: 8-bit signed (2k) data
 	);
 end sa;
 
 architecture behavior of sa is
 	component mac is	-- multiply-accumulate unit
 		port (
-			a : in std_logic_vector(7 downto 0);	-- input: 1: 8-bit signed (2k) data
-			b : in std_logic_vector(7 downto 0);	-- input: 2: 8-bit signed (2k) data
+			a : in std_logic_vector((bitSize-1) downto 0);	-- input: 1: 8-bit signed (2k) data
+			b : in std_logic_vector((bitSize-1) downto 0);	-- input: 2: 8-bit signed (2k) data
 			c : in std_logic;						-- clock
 			r : in std_logic;						-- reset (to "00000000")
-			d : out std_logic_vector(7 downto 0);	-- output 8-bit data (sum of: "a * b")
-			e : out std_logic_vector(7 downto 0);	-- output: 1: 8-bit signed (2k) data (= a: delayed by one clock cycle)
-			f : out std_logic_vector(7 downto 0)	-- output: 2: 8-bit signed (2k) data (= b: delayed by one clock cycle)
+			d : out std_logic_vector((bitSize-1) downto 0);	-- output 8-bit data (sum of: "a * b")
+			e : out std_logic_vector((bitSize-1) downto 0);	-- output: 1: 8-bit signed (2k) data (= a: delayed by one clock cycle)
+			f : out std_logic_vector((bitSize-1) downto 0)	-- output: 2: 8-bit signed (2k) data (= b: delayed by one clock cycle)
 		);
 	end component mac;
 	
-	signal ver : std_2d_vector_array(0 to array_size-1, 0 to array_size);	-- vertical connections
-	signal hor : std_2d_vector_array(0 to array_size, 0 to array_size-1);	-- horizontal connections
+	signal ver : std_2d_vector_array(0 to systolicArraySize-1, 0 to systolicArraySize);	-- vertical connections
+	signal hor : std_2d_vector_array(0 to systolicArraySize, 0 to systolicArraySize-1);	-- horizontal connections
 	
 begin
 	gen1 : FOR i IN 0 TO systolicArraySize-1 GENERATE
