170a3abb45b98e35c958e61d27b8f3c0ed6ded20
Gero Grühn <gero.gruehn@tu-dortmund.de>
2022-03-22 08:25:21 +0100

    fifo aus dem sa_filler gepackt und sa_filler korrigiert

--- VHDL/sa_filler.vhdl
+++ VHDL/sa_filler.vhdl
@@ -18,9 +18,8 @@ entity sa_filler is
 		readIn : in std_logic;
 		writeOut : in std_logic;
 		rst : in std_logic;
-		wdata : in std_logic_vector((bitSize-1) downto 0);
-		winc, wclk, wrst_n : in std_logic;
-		wfull : out std_logic;
+		fifoOut : in std_logic_vector((bitSize-1) downto 0);
+		rinc : out std_logic;
 		rdy : out std_logic;
 		saIn : out std_1d_vector_array(0 to systolicArraySize-1)
 	);
@@ -48,21 +47,6 @@ architecture behaviour of sa_filler is
 	);
     	end component stack_filler;
 ---------------------------------------
-	component fifo is
-	generic(
-		dataSize:integer:=8;
-		addressSize:integer:=8
-	);
-    	port(
-        	rdata: out std_logic_vector(dataSize-1 downto 0);
-        	wfull: out std_logic;
-        	rempty: out std_logic;
-        	wdata: in std_logic_vector(dataSize-1 downto 0);
-        	winc, wclk, wrst_n: in std_logic;
-        	rinc, rclk, rrst_n: in std_logic
-    	);
-	end component fifo;
----------------------------------------
 	component and_gate_two is
 	port (		
         	a,b : in std_logic;
@@ -71,17 +55,13 @@ architecture behaviour of sa_filler is
 	end component and_gate_two;
 ---------------------------------------
 type array_std_logic_vector is array(0 to systolicArraySize-1) of std_logic_vector((counterSize-1) downto 0);
-signal fifoOut : std_logic_vector((bitSize-1) downto 0);
 signal rdys,en,enNxt : std_logic_vector(0 to (systolicArraySize-1));
-signal andOut,rst_fifo,rempty : std_logic;
 signal addressSize:integer;
 signal numZeros : array_std_logic_vector;
 begin
-	addressSize <= integer(ceil(log2(real(systolicArraySize**2))))+1;
+	addressSize <= integer(ceil(log2(real(systolicArraySize**2))));
 	en(0) <= readIn;
-	rst_fifo <= '1';--for the moment we just dont want to reset the fifos readpointer
-	and1 : and_gate_two port map(a=>readIn,b=>rdys(systolicArraySize-1),o=>andOut);
-	fifo1 : fifo generic map(dataSize=>bitSize,addressSize=>addressSize) port map(rdata=>fifoOut,wfull=>wfull,rempty=>rempty,wdata=>wdata,winc=>winc,wclk=>wclk,wrst_n=>wrst_n,rinc=>andOut,rclk=>clk,rrst_n=>rst_fifo);
+	and1 : and_gate_two port map(a=>readIn,b=>rdys(systolicArraySize-1),o=>rinc);
 	gen1 : FOR i IN 0 TO systolicArraySize-1 GENERATE
 		numZeros(i)<=std_logic_vector(to_unsigned(i, counterSize));
 		lower_bit: if i/=0 generate
